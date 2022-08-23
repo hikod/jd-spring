9b37270f99003f1a9ce90557cb786bb6ac1e74b8
hikod <hikmetdogru00@gmail.com>
2022-08-23 11:43:34 -0400

    Tightly coupled concept example

---
+++ Spring-00-Coupling/src/main/java/com/cydeo/tightly_coupled/MyApp.java
@@ -0,0 +1,13 @@
+package com.cydeo.tightly_coupled;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+        FullTimeMentor fullTime = new FullTimeMentor();
+        PartTimeMentor partTime = new PartTimeMentor();
+
+        Mentor mentor = new Mentor(fullTime, partTime);
+
+        mentor.manageAccount();
+    }
+}
