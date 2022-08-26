b01c4833c902f425c5106db36a4a26e2cfaf3136
hikod <hikmetdogru00@gmail.com>
2022-08-26 18:57:19 -0400

    @Lazy

--- Spring-05-Core-BeanScrope/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-05-Core-BeanScrope/src/main/java/com/cydeo/service/CommentService.java
@@ -4,11 +4,13 @@ import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 // @Scope("prototype") //  use the different object in the memory
+// @Lazy creates the object when it is being called
 public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
