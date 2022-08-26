8ea2f6bd27f0cba3d1ce58155225537deaff8df5
hikod <hikmetdogru00@gmail.com>
2022-08-26 05:15:01 -0400

    handling multiple beans

--- Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/MyApp.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/MyApp.java
@@ -7,7 +7,16 @@ public class MyApp {
     public static void main(String[] args) {
 
         ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class);
-         FullTimeMentor ftm = container.getBean(FullTimeMentor.class);
+        // container can hold many Configuration files
+        // ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class, ConfigAny.class);
+
+        FullTimeMentor ftm = container.getBean(FullTimeMentor.class);
+
+        // p1 refers to the object in ConfigApp
+        PartTimeMentor ptm = container.getBean("p1", PartTimeMentor.class);
+
+
         ftm.createAccount();
+        ptm.createAccount();
     }
 }
