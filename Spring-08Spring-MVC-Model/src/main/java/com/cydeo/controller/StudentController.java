8999344eaffe035ddb1ca4a3c6b1ec08abc8061a
hikod <hikmetdogru00@gmail.com>
2022-09-04 08:15:20 -0400

    Spring MVC - Model adding student model

--- Spring-08Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,8 +1,12 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class StudentController {
@@ -11,6 +15,16 @@ public class StudentController {
     public String homePage(Model model){
         model.addAttribute("firstName", "John");
         model.addAttribute("lastName", "Doe");
+
+        List<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(7);
+        list.add(10);
+        list.add(1);
+        model.addAttribute("numbers", list);
+
+        Student student = new Student(1, "Jane", "Doe");
+        model.addAttribute("student",student);
 
         return "student/welcome";
     }
