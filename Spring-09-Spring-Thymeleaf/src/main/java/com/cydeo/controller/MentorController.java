ef8bee4b8808943173f7de5a580fe298b36240d5
hikod <hikmetdogru00@gmail.com>
2022-09-13 23:59:36 -0400

    Post mapping

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,7 +4,11 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
@@ -13,8 +17,14 @@ public class MentorController {
     @GetMapping("register")
     public String showForm(Model model){
         model.addAttribute("mentor", new Mentor());
+        List<String> batchList = Arrays.asList("JD1","JD2","EU1","EU2","B18","B20");
+        model.addAttribute("batchList", batchList);
         return "/mentor/mentor-register";
     }
 
+    @PostMapping("/confirm")
+    public String showForm2(Model model){
+        return "/mentor/mentor-confirmation";
+    }
 
 }
