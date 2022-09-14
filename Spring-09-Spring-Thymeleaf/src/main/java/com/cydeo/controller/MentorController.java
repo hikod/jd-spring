ad3f636003864097c45dbb9f95831755eac30442
hikod <hikmetdogru00@gmail.com>
2022-09-13 21:50:12 -0400

    adding mentor controller & thymeleaf

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -0,0 +1,19 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @GetMapping("register")
+    public String showForm(Model model){
+
+        return "/mentor/mentor-register";
+    }
+
+
+}
