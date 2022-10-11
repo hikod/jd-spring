754701b591eb660051e4d5f496881eb62272e9d0
hikod <hikmetdogru00@gmail.com>
2022-10-11 05:09:20 +0300

    adding gender enum and column annotation

--- Spring-10-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -1,2 +1,5 @@
-INSERT INTO EMPLOYEES (id,name)
-VALUES ( 1, 'Mike Smith' );
+-- INSERT INTO EMPLOYEES (id,name)
+-- VALUES ( 1, 'Mike Smith' );
+
+INSERT INTO student (first_name, last_name, email)
+VALUES ( "Jane", "Doe", "janedoe@test.com" )
