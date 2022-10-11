575846d01624f09d5bec00f6addc86672f979d95
hikod <hikmetdogru00@gmail.com>
2022-10-11 05:39:59 +0300

    adding Car entity

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Car.java
@@ -0,0 +1,20 @@
+package com.cydeo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String make;
+    private String model;
+
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+}
