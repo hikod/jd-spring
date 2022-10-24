76aceeb0384825ea0be1a7d460b220824f239046
hikod <hikmetdogru00@gmail.com>
2022-10-24 02:13:27 -0400

    adding OneToMany relationship and creating Merchant entity

---
+++ Spring-12-ORM-OneToMany-ManyToOne/src/main/java/com/cydeo/model/Merchant.java
@@ -0,0 +1,27 @@
+package com.cydeo.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "merchants")
+@NoArgsConstructor
+@Data
+public class Merchant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+    private BigDecimal transactionFee;
+    private BigDecimal commissionRate;
+    private Integer payoutDelayCount;
+
+    @OneToMany(mappedBy = "merchant") // in OneToMany relationship ownership belongs to Many side
+    private List<Payment> payment;
+}
