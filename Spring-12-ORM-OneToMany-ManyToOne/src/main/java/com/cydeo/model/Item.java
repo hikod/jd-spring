fc90b57dd59bbbf2607f669c60ec8f25eea95bfb
hikod <hikmetdogru00@gmail.com>
2022-10-24 03:30:12 -0400

    adding Cart and Item entities as well as repositories

---
+++ Spring-12-ORM-OneToMany-ManyToOne/src/main/java/com/cydeo/model/Item.java
@@ -0,0 +1,20 @@
+package com.cydeo.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "items")
+@NoArgsConstructor
+@Data
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+
+}
